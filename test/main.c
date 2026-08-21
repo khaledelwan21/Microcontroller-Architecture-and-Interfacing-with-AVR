29bb9591a0c4368e24279e9f4d486aa0e5cbc973
Khaled Elwan <khaledelwan2004@gmail.com>
2026-08-21 14:37:05 +0300

    First commit

---
+++ test/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 21, 2026
+ *      Author: khaled
+ */
+
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRD |= (1 << PD1);   // PB0 Output
+
+    while (1)
+    {
+
+        PORTD &= ~(1 << PD1);  // LED OFF
+
+    }
+}
