9f872d125219aa66f462d0fe6bc730907f42e78d
Khaled Elwan <khaledelwan2004@gmail.com>
2026-08-26 06:35:07 +0300

    added CLCD DRIVER

---
+++ CTOS/ATmega32/HAL/CLCD/CLCD_private.h
@@ -0,0 +1,13 @@
+/*
+ *  file Name : LCD_Private.h
+ *  Created on: ??þ/??þ/???? at ?:??:?? Õ
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#ifndef HAL_LCD_LCD_PRIVATE_H_
+#define HAL_LCD_LCD_PRIVATE_H_
+
+
+
+#endif /* HAL_LCD_LCD_PRIVATE_H_ */
