b8b1a4b8573e377d7a8ebe9ceb4d4f75a9eec466
Khaled Elwan <khaledelwan2004@gmail.com>
2026-09-08 12:27:11 +0300

    update TWI update

--- CTOS/ATmega32/MCAL/TWI/TWI_config.h
+++ CTOS/ATmega32/MCAL/TWI/TWI_config.h
@@ -4,3 +4,24 @@
 * Date: 7/9/2026 
 - Version: 1.0
 ----------------------------------------------------------------*/
+
+#ifndef TWI_CONFIG_H_
+#define TWI_CONFIG_H_
+
+#define F_CPU 8000000UL
+
+
+/*
+	$ Options :-
+		1-PRESCALLER_BY_1
+		2-PRESCALLER_BY_4
+		3-PRESCALLER_BY_16
+		4-PRESCALLER_BY_64
+*/
+#define TWI_PRESCALLER  PRESCALLER_BY_1
+
+
+/*Set SCL frequency to 400kHz, with 8Mhz system frequency*/
+#define SCL_Clock  400000UL 
+
+#endif /* TWI_CONFIG_H_ */
--- CTOS/ATmega32/MCAL/TWI/TWI_interface.h
+++ CTOS/ATmega32/MCAL/TWI/TWI_interface.h
@@ -4,3 +4,44 @@
 * Date: 7/9/2026 
 - Version: 1.0
 ----------------------------------------------------------------*/
+
+#ifndef TWI_INTERFACE_H_
+#define TWI_INTERFACE_H_
+/* Error Status fot TWI */
+typedef enum
+{
+	NoError,
+	StartConditionErr,
+	RepeatedStartError,
+	SlaveAddressWithWriteErr,
+	SlaveAddressWithReadErr,
+	MasterWriteByteErr,
+	MasterReadByteErr,
+	SlaveWriteByteErr,
+	SlaveReadByteErr,
+}TWI_ErrStatus;
+
+/*Set master address to 0 if master will not be addressed*/
+void TWI_voidInitMaster(u8 Copy_u8Address);
+
+void TWI_voidInitSlave(u8 Copy_u8Address);
+
+TWI_ErrStatus TWI_SendStartCondition(void);
+
+TWI_ErrStatus TWI_SendRepeatedStart(void);
+
+TWI_ErrStatus TWI_SendSlaveAddressWithWrite(u8 Copy_u8SlaveAddress);
+
+TWI_ErrStatus TWI_SendSlaveAddressWithRead(u8 Copy_u8SlaveAddress);
+
+TWI_ErrStatus TWI_MasterWriteDataByte(u8 Copy_u8DataByte);
+
+TWI_ErrStatus TWI_MasterReadDataByte(u8* Copy_pu8DataByte);
+
+TWI_ErrStatus TWI_SlaveWriteDataByte(u8 Copy_u8DataByte);
+
+TWI_ErrStatus TWI_SlaveReadDataByte(u8* Copy_pu8DataByte);
+
+
+void TWI_SendStopCondition(void);
+#endif /* TWI_INTERFACE_H_ */
